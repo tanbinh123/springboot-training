075c6798fcbf9544d8fa0b06f804fbe966c5b066
gaosong <805940564@qq.com>
2018-06-16 00:12:50 +0800

    springboot训练

---
+++ springboot-properties/src/main/java/com/gsonkeno/springbootproperties/SpringbootPropertiesApplication.java
@@ -0,0 +1,26 @@
+package com.gsonkeno.springbootproperties;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * CommandLineRunner接口类似于Spring中的ApplicationListener接口
+ * SpringBoot容器启动完成后，将会执行接口中定义的run方法
+ */
+@SpringBootApplication
+public class SpringbootPropertiesApplication implements CommandLineRunner {
+
+    @Autowired
+    private HomeProperties homeProperties;
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootPropertiesApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println(homeProperties);
+    }
+}
