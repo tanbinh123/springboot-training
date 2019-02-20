66a9f2681812e9ac25effea103ba85989fdbba30
高嵩 <gaosong@vvic.com>
2019-02-20 19:36:36 +0800

    增加feign客户端测试

--- springboot-eureka-client/src/test/java/com/gsonkeno/springbooteurekaclient/DemoApplicationTests.java
+++ springboot-eureka-client/src/test/java/com/gsonkeno/springbooteurekaclient/DemoApplicationTests.java
@@ -2,6 +2,7 @@ package com.gsonkeno.springbooteurekaclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DemoApplicationTests {
 
+    @Autowired
+    private CallHelloService1 callHelloService1;
+
     @Test
     public void contextLoads() {
+        String hehe = callHelloService1.hello("hehe");
+        System.out.println(hehe);
     }
 
 }
