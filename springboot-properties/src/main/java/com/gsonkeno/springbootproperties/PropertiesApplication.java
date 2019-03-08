f996573c3a450e5da4bcf3abcfa5ba411ce57beb
高嵩 <gaosong@vvic.com>
2019-03-08 15:12:09 +0800

    add springboot-cache module

--- springboot-properties/src/main/java/com/gsonkeno/springbootproperties/PropertiesApplication.java
+++ springboot-properties/src/main/java/com/gsonkeno/springbootproperties/PropertiesApplication.java
@@ -3,20 +3,28 @@ package com.gsonkeno.springbootproperties;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ConfigurableApplicationContext;
+
+import java.util.Iterator;
 
 /**
  * CommandLineRunner接口类似于Spring中的ApplicationListener接口
  * SpringBoot容器启动完成后，将会执行接口中定义的run方法
  */
 @SpringBootApplication
-public class SpringbootPropertiesApplication implements CommandLineRunner {
+@EnableConfigurationProperties(CompanyProperties.class)
+public class PropertiesApplication implements CommandLineRunner {
 
 //    @Autowired
 //    private HomeProperties homeProperties;
 
     public static void main(String[] args) {
-        ConfigurableApplicationContext context = SpringApplication.run(SpringbootPropertiesApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(PropertiesApplication.class, args);
+        Iterator<String> beanNames = context.getBeanFactory().getBeanNamesIterator();
+        while (beanNames.hasNext()){
+            System.out.println(beanNames.next());
+        }
 
     }
 
