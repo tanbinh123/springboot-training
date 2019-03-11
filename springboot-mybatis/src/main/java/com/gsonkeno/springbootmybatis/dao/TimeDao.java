4d634db7086b2fa83660664bdfac02fef0db134b
gaosong <805940564@qq.com>
2019-03-12 00:02:09 +0800

    增加jsonp测试案例

---
+++ springboot-mybatis/src/main/java/com/gsonkeno/springbootmybatis/dao/TimeDao.java
@@ -0,0 +1,8 @@
+package com.gsonkeno.springbootmybatis.dao;
+
+import java.util.List;
+import java.util.Map;
+
+public interface TimeDao {
+    List<Map> selectAll();
+}
--- springboot-mybatis/src/test/java/com/gsonkeno/springbootmybatis/SpringbootMybatisApplicationTests.java
+++ springboot-mybatis/src/test/java/com/gsonkeno/springbootmybatis/SpringbootMybatisApplicationTests.java
@@ -1,5 +1,6 @@
 package com.gsonkeno.springbootmybatis;
 
+import com.gsonkeno.springbootmybatis.dao.TimeDao;
 import com.gsonkeno.springbootmybatis.domain.City;
 import com.gsonkeno.springbootmybatis.service.CityService;
 import org.junit.Test;
@@ -8,6 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+import java.util.Map;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SpringbootMybatisApplicationTests {
@@ -15,10 +19,19 @@ public class SpringbootMybatisApplicationTests {
     @Autowired
     private CityService cityService;
 
+    @Autowired
+    private TimeDao timeDao;
+
     @Test
     public void contextLoads() {
         City xinyang = cityService.findCityByName("xinyang");
         System.out.println(xinyang);
+    }
+
+    @Test
+    public void test(){
+        List<Map> maps = timeDao.selectAll();
+        System.out.println(maps);
     }
 
 }
