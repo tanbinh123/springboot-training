441734eca4f8c85fff52229a202427a9ada423ab
高嵩 <gaosong@vvic.com>
2019-05-30 20:33:01 +0800

    spring eurake config

--- springboot-session/src/main/java/com/gsonkeno/springbootsession/SpringbootSessionApplication.java
+++ springboot-session/src/main/java/com/gsonkeno/springbootsession/SpringbootSessionApplication.java
@@ -2,8 +2,10 @@ package com.gsonkeno.springbootsession;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication
+@EnableRedisHttpSession(maxInactiveIntervalInSeconds = 1800)
 public class SpringbootSessionApplication {
 
     public static void main(String[] args) {
